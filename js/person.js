3ed915c14eea523d64be0349cd2eaf03f29682d4
kucseranna <kucseranna@gmail.com>
2021-10-28 16:44:53 +0200

    tadamm!

--- js/person.js
+++ js/person.js
@@ -1,10 +1,10 @@
 
-const personDatalog = (pdlog) => {
+const personDataLog = (pdlog) => {
 pdlog.firstname = pdlog.firstname || 'John';
 pdlog.lastname = pdlog.lastname || 'Doe';
 pdlog.age = pdlog.age || '33';
 
-return "My name is " + pdlog.firstname + " " + pdglog.lastname + ". I'm " + pdglog.age + " years old."
+return "My name is " + pdlog.firstname + " " + pdlog.lastname + ". I'm " + pdlog.age + " years old."
 }
 
     
