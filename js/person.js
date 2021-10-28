7b7f5db17afc91edf3e1c4c0ed653cdd14432614
kucseranna <kucseranna@gmail.com>
2021-10-28 15:10:38 +0200

    Evelinnek

---
+++ js/person.js
@@ -0,0 +1,10 @@
+
+const personDatalog = (pdlog) => {
+pdlog.firstname = pdlog.firstname || 'John';
+pdlog.lastname = pdlog.lastname || 'Doe';
+pdlog.age = pdlog.age || '33';
+
+return "My name is " + pdlog.firstname + " " + pdglog.lastname + ". I'm " + pdglog.age + " years old."
+}
+
+    
---
+++ js/sum.js
@@ -0,0 +1,11 @@
+const summation = (a = 0, b = 0) => {
+    return a + b;
+};
+
+const subtraction = (a = 0, b = 0) => {
+    return a - b;
+};
+
+//akkor lássuk...
+// console.log(summation(3,22));
+// console.log(subtraction( 4, 14));
